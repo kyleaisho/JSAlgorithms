877e812e426b4d791900b9ed1650e24aac521b7b
Kyle Aisho <kyleaisho@gmail.com>
2019-09-16 07:55:59 -0700

    In place quick sort

--- 10_sorting/quick_sort.js
+++ 10_sorting/quick_sort.js
@@ -1,39 +1,46 @@
-function swap(a, idx1, idx2) {
-  const tmp = a[idx1];
-  a[idx1] = a[idx2];
-  a[idx2] = tmp;
+function swap(a, i1, i2) {
+  const tmp = a[i1];
+  a[i1] = a[i2];
+  a[i2] = tmp;
 }
 
-function quicksort(arr, left, right) {
-  if (!arr.length) return arr;
+/**
+ * Select the last element to be the pivot
+ * Keep two pointers firsthigh and left incrementer
+ * 
+ * First high increments when the current value is less than the pivot
+ * it should represent the fist index which has a value greater than the pivot
+ * 
+ * The last step in the algorithm after the swaps is to swap the pivot
+ * into the firsthigh index and move firsthigh's value to the end, this
+ * keeps the invariant that eveything to the right of the pivot is > and everything
+ * to the left is <
+ */
+function partition(a, l, h) {
+  let p = h;
+  let firsthigh = l;
 
-  left = left || 0;
-  right = right || arr.length - 1;
-
-  const pivotIdx = Math.floor((arr.length - 1) / 2);
-  const pivot = arr[pivotIdx];
-
-  while (left < right) {
-    while (arr[left] <= pivot && left < pivotIdx) left++;
-    while (arr[right] >= pivot && right > pivotIdx) right--;
-
-    if (arr[left] > arr[right]) {
-      swap(arr, left, right);
-      left++;
-      right--;
+  for (let i = l; i < h; i++) {
+    if (a[i] < a[p]) {
+      swap(a, i, firsthigh);
+      firsthigh++;
     }
   }
 
-  const leftArray = arr.slice(0, pivotIdx);
-  const rightArray = arr.slice(pivotIdx + 1);
+  swap(a, firsthigh, p);
 
-  return (
-    quicksort(leftArray)
-      // .concat([pivot])
-      .concat(arr[pivotIdx], quicksort(rightArray))
-  );
+  return firsthigh;
 }
 
-const a = [2, 4, 1, 7, 5, 2, 9, 7];
-quicksort(a);
-console.log(a);
+
+function qsort(a, l, h) {
+  if (l < h) {
+    const p = partition(a, l, h);
+    qsort(a, l, p - 1);
+    qsort(a, p + 1, h);
+  }
+}
+
+const arr = [ 4, 1, 2, 5, 9, 2, 9, 7];
+qsort(arr, 0, arr.length - 1);
+console.log(arr);
