898de2046cf80e3f73865d2e248b817e58733225
Kyle Aisho <kyleaisho@gmail.com>
2019-10-21 07:53:00 -0700

    Median 2 sorted arrays

--- LeetCode/median_two_arrays.js
+++ LeetCode/median_two_arrays.js
@@ -4,66 +4,45 @@
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
-    if (nums1.length < 3 && nums2.length < 3) {
-        return median(mergeSorted(nums1, nums2));
+    if (nums1.length > nums2.length) {
+        return findMedianSortedArrays(nums2, nums1);
     }
-
-    if (nums1.length < 1) return median(nums2);
-    if (nums2.length < 1) return median(nums1);
     
-    const mid1 = Math.floor((nums1.length - 1) / 2);
-    const mid2 = Math.floor((nums2.length - 1) / 2);
-    const median1 = median(nums1);
-    const median2 = median(nums2);
+    const x = nums1.length;
+    const y = nums2.length;
     
-    if (median1 === median2) return median1;
+    let low = 0;
+    let high = x;
     
-    if (median1 < median2) {
-        return findMedianSortedArrays(nums1.slice(mid1), nums2.slice(0, mid2 + 1));
-    } else {
-        return findMedianSortedArrays(nums1.slice(0, mid1 + 1), nums2.slice(mid2));
-    }
-};
-
-const median = (arr) => {
-    const mid = Math.floor((arr.length - 1) / 2);
-    
-    if (arr.length % 2 === 0) {
-        // even
-        const right = mid + 1;
-
-        return (arr[mid] + arr[mid + 1]) / 2;
-    } else {
-        return arr[mid];
-    }
-}
-
-const getResult = (nums1, nums2) => {
-    const merge = mergeSorted(nums1, nums2);
-    return median(merge);
-};
-
-const mergeSorted = (nums1, nums2) => {
-    const ret = [];
-    
-    while (nums1.length && nums2.length) {
-        if (nums1[0] <= nums2[0]) {
-            ret.push(nums1.shift());
+    while (low <= high) {
+        const partitionX = Math.floor((low + high) / 2);
+        const partitionY = Math.floor((x + y + 1) / 2) - partitionX;
+        
+        const maxLeftX = (partitionX === 0) ? Number.NEGATIVE_INFINITY : nums1[partitionX - 1];
+        const minRightX = (partitionX === x) ? Number.POSITIVE_INFINITY : nums1[partitionX];
+        const maxLeftY = (partitionY === 0) ? Number.NEGATIVE_INFINITY : nums2[partitionY - 1];
+        const minRightY = (partitionY === y) ? Number.POSITIVE_INFINITY : nums2[partitionY];
+        
+        if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+            // median found
+            return (x + y) % 2 === 0 ?
+                (Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY)) / 2 :
+                Math.max(maxLeftX, maxLeftY);
+        }
+        
+        if (maxLeftX > minRightY) {
+            // too far to the right
+            high = partitionX - 1;
         } else {
-            ret.push(nums2.shift());
+            low = partitionX + 1;
         }
     }
-    
-    if (nums1.length) {
-        nums1.forEach(n => ret.push(n));
-    } else {
-        nums2.forEach(n => ret.push(n));
-    }
-    
-    return ret;
+    throw 'fuck'
 }
 
-const arr1 = [1,2];
-const arr2 = [1,2,3];
+// const arr1 = [1,3,8,9,15];
+// const arr2 = [7,11,18,19,21,25];
+const arr1 = [1,3];
+const arr2 = [2];
 
 console.log(findMedianSortedArrays(arr1, arr2))
