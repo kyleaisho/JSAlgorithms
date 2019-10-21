726abb4a327d1b15635acf96c6f785930b782915
Kyle Aisho <kyleaisho@gmail.com>
2019-10-20 17:18:49 -0700

    Incomplete median 2 sorted arrays

--- LeetCode/median_two_arrays.js
+++ LeetCode/median_two_arrays.js
@@ -5,29 +5,28 @@
  */
 var findMedianSortedArrays = function(nums1, nums2) {
     if (nums1.length < 3 && nums2.length < 3) {
-        return median(nums1, nums2);
+        return median(mergeSorted(nums1, nums2));
     }
+
+    if (nums1.length < 1) return median(nums2);
+    if (nums2.length < 1) return median(nums1);
     
-    const mid1 = Math.floor(nums1.length / 2);
-    const mid2 = Math.floor(nums2.length / 2);
+    const mid1 = Math.floor((nums1.length - 1) / 2);
+    const mid2 = Math.floor((nums2.length - 1) / 2);
     const median1 = median(nums1);
     const median2 = median(nums2);
     
     if (median1 === median2) return median1;
     
     if (median1 < median2) {
-        // go right for 1
-        // go left for 2
         return findMedianSortedArrays(nums1.slice(mid1), nums2.slice(0, mid2 + 1));
     } else {
-        // go ledt for 1
-        // go right for 2
         return findMedianSortedArrays(nums1.slice(0, mid1 + 1), nums2.slice(mid2));
     }
 };
 
 const median = (arr) => {
-    const mid = Math.floor(arr.length / 2);
+    const mid = Math.floor((arr.length - 1) / 2);
     
     if (arr.length % 2 === 0) {
         // even
@@ -49,9 +48,9 @@ const mergeSorted = (nums1, nums2) => {
     
     while (nums1.length && nums2.length) {
         if (nums1[0] <= nums2[0]) {
-            ret.push(nums1.pop());
+            ret.push(nums1.shift());
         } else {
-            ret.push(nums2.pop());
+            ret.push(nums2.shift());
         }
     }
     
@@ -63,3 +62,8 @@ const mergeSorted = (nums1, nums2) => {
     
     return ret;
 }
+
+const arr1 = [1,2];
+const arr2 = [1,2,3];
+
+console.log(findMedianSortedArrays(arr1, arr2))
