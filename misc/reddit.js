d2928b23fe28b788c8a4c399adc29b05cdc0653b
Kyle Aisho <kyleaisho@gmail.com>
2019-11-23 13:44:37 -0800

    Updated answer for reddit

--- misc/reddit.js
+++ misc/reddit.js
@@ -9,7 +9,7 @@
 
  (9)
  */
-
+const { isEqual } = require('lodash')
 
 function createMapping(listNodes) {
     const map = {};
@@ -40,14 +40,18 @@ function flatten(map, parent, index, depth) {
     const nodes = map[parent];
     if (!parent || !nodes) return;
 
-    if (depth >= 5) {
+    if (depth > 5) {
         map[parent] = nodes.slice(0, index - 1);
         map[null].push(...nodes.slice(index - 1));
         return;
     }
 
+    while (nodes.length + depth > 5) {
+        map[null].push(nodes.shift());
+    }
+
     nodes.forEach((child, i) => {
-        flatten(map, child, i, depth + 1);
+        flatten(map, child, i, depth + nodes.length);
     });
 }
 
@@ -62,5 +66,24 @@ function flatten(map, parent, index, depth) {
      [1, null],
      [9, null]
  ];
- 
-console.log(createMapping(listNodes));
+
+ const listNodes2 = [
+    [2,1],
+    [3,2],
+    [4,3],
+    [11,3],
+    [12,3],
+    [5,4],
+    [6,4],
+    [7,6],
+    [8, 7],
+    [13, 7],
+    [14, 7],
+    [1, null],
+    [9, null]
+];
+
+console.log(createMapping(listNodes))
+console.assert(isEqual(createMapping(listNodes), [1,9,7]), 'Not equal');
+console.log(createMapping(listNodes2))
+console.assert(isEqual(createMapping(listNodes2), [ 1, 9, 4, 8, 13 ]), 'Not equal');
